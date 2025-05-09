c882855b120e2283b712b4cb4e6b9f0c1fad1b3f
AlexanderLukashuk <upiter465@gmail.com>
2025-05-09 21:23:09 +0500

    update config

---
+++ config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+ALLOWED_CREATORS = os.getenv("ALLOWED_CREATORS").split(",")
