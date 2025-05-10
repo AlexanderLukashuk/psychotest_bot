d66369e9a8fc82dd1822698d99bd91846f31fdf5
AlexanderLukashuk <upiter465@gmail.com>
2025-05-10 14:52:44 +0500

    switch on polling

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 import asyncio
-from fastapi import FastAPI, Request
 from aiogram import Bot, Dispatcher
-from aiogram.types import Update
 from config import BOT_TOKEN
 from bot.handlers import common, test_creation, test_deleting, test_passing
 
-app = FastAPI()
 
-bot = Bot(BOT_TOKEN)
-dp = Dispatcher()
+async def main():
+    bot = Bot(BOT_TOKEN)
+    dp = Dispatcher()
 
-dp.include_router(common.router)
-dp.include_router(test_creation.router)
-dp.include_router(test_passing.router)
-dp.include_router(test_deleting.router)
+    dp.include_router(common.router)
+    dp.include_router(test_creation.router)
+    dp.include_router(test_passing.router)
+    dp.include_router(test_deleting.router)
 
-@app.post("/webhook")
-async def telegram_webhook(req: Request):
-    data = await req.json()
-    update = Update.model_validate(data)
-    await dp.feed_update(bot, update)
-    return {"status": "ok"}
+    await dp.start_polling(bot)
 
-@app.on_event("startup")
-async def on_startup():
-    # Устанавливаем webhook на Vercel URL
-    webhook_url = "https://psychotest-bot.vercel.app/webhook"
-    await bot.set_webhook(webhook_url)
-
-@app.on_event("shutdown")
-async def on_shutdown():
-    await bot.delete_webhook()
+if __name__ == "__main__":
+    asyncio.run(main())
